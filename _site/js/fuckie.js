59fbc626a80ae1b491c47c1d555de5ce100b70c5
zhuowenli <531840344@qq.com>
2014-12-09 12:11:07 +0800

    add frontend and design and diary pages

---
+++ _site/js/fuckie.js
@@ -0,0 +1,8 @@
+function(){
+	var con = document.getElementById("container");
+	con.innerHTML = '还用这么垃圾的IE浏览器，你家人造么？<br />赶紧<a target="_blank" href="http://windows.microsoft.com/zh-cn/internet-explorer/download-ie">升级IE</a>浏览器，或者使用 <a target="_blank" href="http://www.google.cn/intl/zh-CN/chrome/">chrome</a> or <a target="_blank" href="http://www.firefox.com.cn/">Firefox</a> 吧！';
+	con.style.textAlign = "center";
+	con.style.paddingTop = "300px";
+	con.style.fontSize  = "32px";
+	document.body.style.background = "#fff";
+}();
