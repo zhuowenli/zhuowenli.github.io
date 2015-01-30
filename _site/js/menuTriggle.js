7b565ed391b7b3486edbc9d69ed23ad3f50f1424
zhuowenli <531840344@qq.com>
2015-01-30 12:42:37 +0800

    change off-canvas menu with SVG style

--- _site/js/menuTriggle.js
+++ _site/js/menuTriggle.js
@@ -2,14 +2,27 @@
 * @Author: 卓文理 zhuowenli.sinaapp.com
 * @Email:  531840344@qq.com
 * @Date:   2014-11-22 10:19:22
-* @Last Modified by:   zwl
-* @Last Modified time: 2014-12-20 21:31:16
+* @Last Modified by:   卓文理 www.zwlme.com
+* @Last Modified time: 2015-01-30 11:59:24
 */
 (function(){
-	var isMenuOpen,
+	var isMenuOpen = false,
 		menu      = document.getElementById('menu'),
 		btnMenu   = document.getElementById('btnMenu'),
-		container = document.getElementById('container');
+		btnClose  = document.getElementById('btnClose'),
+		container = document.getElementById('container'),
+
+		SVGMenu   = document.getElementById('SVGMenu'),
+		svg  = Snap(SVGMenu.querySelector('svg')),
+		path = svg.select('path'),
+		d    = path.attr('d'),
+
+		open = SVGMenu.getAttribute('data-menu-open').split(';'),
+		close = SVGMenu.getAttribute('data-menu-close').split(';'),
+		openTotal  = open.length,
+		closeTotal = close.length,
+		isAnimating = false;
+
 	// 打开菜单
 	function openMenu(){
 		classie.add(container,'menu-open');
@@ -18,29 +31,68 @@
 		classie.remove(container,'menu-open');
 	}
 
-	// 点击任意地方关闭菜单
-	function bodyClickFn(event) {
-		if (!classie.has(event.target,'menu')) {
-			resetMenu();
-			isMenuOpen = false;
-			document.removeEventListener('click',bodyClickFn);
-		}
-	}
-
 	// 菜单操作
 	function menuTriggle(event) {
-		if(isMenuOpen){
-			isMenuOpen = false;
+		if (isAnimating) {
+			return false;
+		};
+		isAnimating = true;
+
+		if( isMenuOpen ){
+			var closePos = 0,
+				nextClose = function(closePos){
+					if (closePos > closeTotal - 1) {
+						isAnimating = false;
+						return;
+					}else{
+						path.animate({'path' : close[closePos]}, closePos === 0 ? 300 : 400, closePos === 0 ? mina.easein : mina.elastic, function() {
+							nextClose(closePos);
+						});
+						closePos++;
+					}
+				};
+
+			nextClose(closePos);
+			// classie.add(container, 'menu-close');
+
 			resetMenu();
+			// classie.remove(container, 'menu-close');
 		}else{
-			isMenuOpen = true;
-			setTimeout(openMenu(),25);
-			event.stopPropagation();
-			event.preventDefault();
-			document.addEventListener('click',bodyClickFn);
+			openMenu();
+			var openPos = 0;
+			var nextOpen = function(openPos){
+					if (isNaN(openPos)) {
+						openPos = 0;
+					};
+					if (openPos > openTotal - 1) {
+						isAnimating = false;
+						return;
+					}else{
+						path.animate( { 'path' : open[openPos] }, openPos === 0 ? 200 : 300, openPos === 0 ? mina.easein : mina.elastic, function() {
+						// path.animate({'path' : open[openPos]}, 250, mina.easeinout, function(){
+							isAnimating = false;
+							nextOpen(openPos);
+						});
+						openPos++;
+					}
+				};
+			nextOpen();
 		}
+		isMenuOpen = !isMenuOpen;
 	}
 
 	// 菜单按钮事件
 	btnMenu.addEventListener('click',menuTriggle);
+	if (btnClose) {
+		btnClose.addEventListener('click',menuTriggle);
+	};
+
+	// close the menu element if the target it´s not the menu element or one of its descendants..
+	container.addEventListener( 'click', function(ev) {
+		var target = ev.target;
+		if( isMenuOpen && target !== btnMenu ) {
+			menuTriggle();
+		}
+	} );
+
 })();
