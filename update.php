b1e26073468211f35f1dd7f13f2dd2b75885bb8f
zhuowenli <531840344@qq.com>
2015-07-29 22:11:45 +0800

    update webhook

--- update.php
+++ update.php
@@ -3,13 +3,13 @@
  * @Author: 卓文理
  * @Date:   2015-07-29 22:02:53
  * @Last Modified by:   卓文理
- * @Last Modified time: 2015-07-29 22:04:39
+ * @Last Modified time: 2015-07-29 22:11:02
  */
 
 error_reporting ( E_ALL );
 
 $dir    = '/home/zhuowenli.github.io/';
-$handle = popen('cd '.$dir.' && git pull 2>&1','r');
+$handle = popen('cd '.$dir.' && git pull origin master 2>&1','r');
 $read   = stream_get_contents($handle);
 
 printf($read);
