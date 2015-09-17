c4728a0434466445afae6375ff50a03063a4bf53
zhuowenli <531840344@qq.com>
2015-09-17 11:10:36 +0800

    添加console.log代码

--- res/js/index.js
+++ res/js/index.js
@@ -23,6 +23,7 @@ var app = {
                 body.addClass('scrollable');
             }, 1500);
         }
+        this.consoleLog();
     },
     loadPage: function(){
         var body = $('body'),
@@ -87,5 +88,8 @@ var app = {
             $(this).removeClass('show-diary');
             $(this).addClass('show-' + _class);
         };
+    },
+    consoleLog: function(){
+        console.log("%c卓文理"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:16em;color:#fff;");
     }
 }
