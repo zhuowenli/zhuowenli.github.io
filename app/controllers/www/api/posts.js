70de57205aedeb661527745a013d816b491862bf
卓文理 <531840344@qq.com>
2016-12-25 14:12:26 +0800

    :memo: 补全文档

--- app/controllers/www/api/posts.js
+++ app/controllers/www/api/posts.js
@@ -66,82 +66,6 @@ exports.init = function(app) {
         this.body = posts;
     });
 
-    router.post('/api/posts', function *() {
-        const postData = this.request.body;
-        const data = {};
-
-        let {title, categories, tags, content, release_at} = postData;
-        let category_id = 3;
-
-        if (categories) {
-            categories = categories.trim();
-            category_id = CATES[categories] || 3;
-        }
-
-        // release_at = new Date(release_at).getTime();
-        content = content ? content.trim() : '';
-        title = title ? title.trim() : '';
-
-        _.assign(data, {
-            title,
-            content,
-            category_id,
-            release_at,
-            priority: 0,
-            status: 0,
-            user_id: 1,
-        });
-
-        let post = Post.forge(data);
-
-        yield post.save();
-
-        if (tags) {
-            tags = tags.split(',');
-        }
-
-        if (tags && tags.length && typeof tags == 'object') {
-            function saveTags(value) {
-                return new Promise((resolve, reject) => {
-                    Tag.forge({
-                            name: value
-                        })
-                        .save()
-                        .then(tag => {
-                            resolve(tag.id);
-                        });
-                });
-            }
-
-            function saveTaglogs(id) {
-                return Taglog.forge({
-                    post_id: post.id,
-                    tag_id: id
-                }).save();
-            }
-
-            yield Promise.map(tags, value => {
-                value = value.trim();
-
-                let tag = Tag.where(qb => {
-                        qb.where('name', value);
-                    })
-                    .fetch()
-                    .then(res => {
-                        if (res && res.id) {
-                            saveTaglogs(res.id);
-                        } else {
-                            saveTags(value).then(id => {
-                                saveTaglogs(id);
-                            });
-                        }
-                    });
-
-                return tag;
-            });
-        }
-    });
-
     router.get('/api/posts/:id', function *() {
         const post = yield Post.where(qb => {
             qb.where('id', this.params.id);
@@ -154,27 +78,6 @@ exports.init = function(app) {
         let view_count = +post.get('view_count');
 
         yield post.save('view_count', view_count + 1);
-
-        this.body = post;
-    });
-
-    router.put('/api/posts/:id', function *() {
-        const post = yield Post.where({
-            id: this.params.id
-        }).fetch();
-
-        if (!post) {
-            this.throw(404);
-        }
-
-        const data = this.request.body;
-        const {category, user} = data;
-
-        delete data.category;
-        delete data.user;
-
-        // save post
-        yield post.save(data, {patch: true});
 
         this.body = post;
     });
--- wiki/doc.md
+++ wiki/doc.md
@@ -6,22 +6,26 @@
 
 请求：
 
-    GET: /posts
+    GET: /api/posts
 
 请求参数：
 
     {
         page        : 1,                  // 页码
         per_page    : 30,                 // 每页返回数量
-        category    : 1                   // 分类ID（可选），0 || '':所有，1:前端，2: 设计，3:生活日志
-        tag         : 123                 // 标签ID（可选）
+        category_id : 1,                  // 分类ID（可选），0 || '':所有，1:前端，2: 设计，3:生活日志
+        tag_id      : 123,                // 标签ID（可选）
+        search      : 'hello world',      // 模糊查询（可选）
+        status      : 1,                  // 文章状态（可选）
+        order_by    : 'id',               // 排序方式（可选）
+        order_status: 'desc',             // 排序方向（可选），asc: 正序，desc: 倒叙
     }
 
 ### 创建文章
 
 请求：
 
-    POST: /posts
+    POST: /api/posts
 
 请求参数：
 
@@ -29,23 +33,25 @@
         author      : '卓文理',            // 作者
         content     : '具体文章内容...',
         title       : '文章标题',
-        category    : '1',                // 分类，1:前端，2: 设计，3:生活日志
+        categories  : 'frontend',         // 分类，frontend:前端，design: 设计，diary:生活日志
         tags        : '前端,html5,ES2015', // 标签，英文逗号「,」分隔
         excerpt     : '文章摘要...',
-        created_time: '1459172082',       // 创建时间
+        release_at  : '1459172082',       // 创建时间
+        status      : 1,                  // 文章状态
+        images      : [{url: xxx, width: xx}] // 图片
     }
 
 ### 获取文章详情
 
 请求：
 
-    GET: /posts/{:id}
+    GET: /api/posts/{:id}
 
 ### 更新文章
 
 请求：
 
-    PUT: /posts/{:id}
+    PUT: /api/posts/{:id}
 
 请求参数：
 
@@ -53,11 +59,12 @@
         author      : '卓文理',            // 作者
         content     : '具体文章内容...',
         title       : '文章标题',
-        category    : '1',                // 分类，1:前端，2: 设计，3:生活日志
+        categories  : 'frontend',         // 分类，frontend:前端，design: 设计，diary:生活日志
         tags        : '前端,html5,ES2015', // 标签，英文逗号「,」分隔
         excerpt     : '文章摘要...',
-        status      : '1',                // 文章状态，0: 待发布, 1: 发布中, 2: 已删除
-        updated_time: '1459172082',       // 更新时间
+        release_at  : '1459172082',       // 创建时间
+        status      : 1,                  // 文章状态
+        images      : [{url: xxx, width: xx}] // 图片
     }
 
 ### 删除文章
