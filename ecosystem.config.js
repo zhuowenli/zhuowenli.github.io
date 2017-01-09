038853c33d50781e52193cede76865b87a674718
卓文理 <531840344@qq.com>
2017-01-09 22:01:45 +0800

    :art: 更新pm2配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,34 +1,22 @@
+/**
+ * @author: 卓文理
+ * @email : 531840344@qq.com
+ * @desc  : Description
+ */
+'use strict';
+
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
-
-    // First application
-    {
-      name      : "blog",
-      script    : "app/app.js",
-      env_production : {
-        NODE_ENV: "production"
-      },
-      exec_mode: "cluster",
-      instances: 4
-    }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : "zhuowenli",
-      host : "45.33.46.78",
-      ref  : "origin/master",
-      repo : "git@github.com:zhuowenli/zhuowenli.github.io.git",
-      path : "/home/zhuowenli/github/zhuowenli.github.io",
-      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
-    }
-  }
+    apps: [
+        {
+            name: "blog",
+            script: "./app/app.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        }
+    ]
 }
